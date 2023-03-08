3352789b5bce1150d04141c938d976080c6d384e
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 18:30:28 -0500

    Add files via upload

---
+++ VariableNames1.java
@@ -0,0 +1,30 @@
+package QAclass;
+
+public class VariableNames1 {
+    public static void main(String[] args) {
+        int var1 = 12546839;
+        System.out.println(var1);
+        long var2 = 23514678525L;
+        System.out.println(var2);
+        float var3 = 1122.345F;
+        System.out.println(var3);
+        double var4 = 3.712638712134787;
+        System.out.println(var4);
+        boolean var5 = true;
+        System.out.println(var5);
+        char var6 = 'S';
+        System.out.println(var6);
+        String var7 = "My Java Class";
+        System.out.println(var7);
+    }
+}
+//int + int = int
+// int + float = float
+// long + int = long
+// double + float = double
+//boolean + int = not possible
+//long + double = double
+//char + int = not possible
+//String
+// int = String = String
+// char + char Computer will not understand and gives asterisk character number
