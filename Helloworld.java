e8b045297bd90e1d17d04fcb3c449f8a1b83c9c1
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 18:29:39 -0500

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+package QAclass;
+
+public class Helloworld {
+
+
+
+        public static void main(String[] args) {
+            System.out.println("my first java class");
+        }
+
+    }
+
+
