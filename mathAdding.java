80af6d9b9f270ca28646551fc677d9e125b1638f
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 18:38:46 -0500

    Add files via upload

---
+++ mathAdding.java
@@ -0,0 +1,8 @@
+package QAclass;
+
+public class mathAdding {
+    public static void main(String[] args) {
+        System.out.println(5+5);
+    }
+}
+//for numbers if you give ("") it never prints the Sum(5+5)without cots
