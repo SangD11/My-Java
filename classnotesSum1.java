021f52f357106e678afdfaa9152fb9b8cbaaa069
SangD11 <116387184+SangD11@users.noreply.github.com>
2023-03-08 18:31:30 -0500

    Add files via upload

---
+++ classnotesSum1.java
@@ -0,0 +1,23 @@
+package QAclass;
+
+import java.sql.SQLOutput;
+
+public class classnotesSum1 {
+    public static void main(String[] args) {
+        // write a program to take 3 int number
+        // add first two numbers and then subtract 3rd number from the sum
+        //print it
+        // int a = 5;
+        // int b = 7;
+        // int c = 3;
+//        System.out.println(result);    }
+        //write a java program that can hold two integer number and sum it.
+        int a = 10;
+        int b = 20;
+        int result = a + b;
+        //System.out.println(result);
+       // System.out.println();
+
+    }
+
+}
